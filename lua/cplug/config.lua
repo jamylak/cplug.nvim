02b400ac8afaf5f94b6d34d5aba9de02599be10e
jamylak <jamylak@gmail.com>
2026-04-03 23:54:25 +1100

    config

---
+++ lua/cplug/config.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+local defaults = {
+  create_commands = true,
+  default_keymaps = true,
+  keymaps = {
+    compile_debug = "<leader>c",
+  },
+}
+
+function M.defaults()
+  return vim.deepcopy(defaults)
+end
+
+function M.resolve(opts)
+  return vim.tbl_deep_extend("force", M.defaults(), opts or {})
+end
+
+return M
